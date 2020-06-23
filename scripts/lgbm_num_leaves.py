18c2c7f60bd5e0009e4f3f8827a12fb7609170be
mavillan <nallivam@gmail.com>
2020-06-23 20:09:29 +0000

    Updates script to tune num_leaves

--- scripts/lgbm_num_leaves.py
+++ scripts/lgbm_num_leaves.py
@@ -10,7 +10,6 @@ import category_encoders as ce
 import matplotlib.pyplot as plt
 import optuna
 
-from tsforest.trend import compute_trend_models
 from tsforest.forecaster import LightGBMForecaster
 from tsforest.utils import make_time_range
 from tsforest.metrics import compute_rmse
@@ -18,8 +17,10 @@ from tsforest.metrics import compute_rmse
 # local modules
 import sys
 sys.path.append("../lib/")
-from utils import compute_scaling, compute_weights, reduce_mem_usage
+from utils import (compute_scaling, compute_weights, reduce_mem_usage, 
+                   compute_scales_by_level, compute_weights_by_level)
 from evaluation import _WRMSSEEvaluator, WRMSSEEvaluator, Evaluator, WRMSSEEvaluatorL12
+from encoding import HierarchicalEncoder
 
 ###########################################################################################
 # logger setting
@@ -42,8 +43,6 @@ data = (pd.read_parquet("../input/train_dataframe.parquet")
 scaling_input = pd.read_parquet("../input/scaling_input.parquet")
 weighting_input = pd.read_parquet("../input/weighting_input.parquet")
 
-data.info()
-
 # periods used for validation
 valid_periods = [
     (pd.to_datetime("2015-04-25"), pd.to_datetime("2015-05-22")),
@@ -52,8 +51,15 @@ valid_periods = [
     (pd.to_datetime("2016-04-25"), pd.to_datetime("2016-05-22")),
 ]
 
+# weights and scales for all the validation periods
+all_scales = dict()
+all_weights = dict()
+for i in range(4):
+    all_scales[i] = compute_scales_by_level(scaling_input, valid_periods[i][0]) 
+    all_weights[i] = compute_weights_by_level(weighting_input, valid_periods[i][0])
+
 ###########################################################################################
-# definition of objective
+# model config
 ###########################################################################################
 
 default_model_params = {
@@ -61,14 +67,14 @@ default_model_params = {
     'tweedie_variance_power': 1.1,
     'metric':'None',
     'num_iterations':100000,
-    'early_stopping_rounds':300,
+    'early_stopping_rounds':200,
     'max_bin': 127,
     'bin_construct_sample_cnt':6000000,
     #'num_leaves': 2**9-1,
     #'min_data_in_leaf': 2**9-1,
-    'learning_rate': 0.03, 
-    'feature_fraction': 0.9,
-    'bagging_fraction':0.66,
+    'learning_rate': 0.02, 
+    'feature_fraction': 0.7,
+    'bagging_fraction':0.9,
     'bagging_freq':1,
     'lambda_l2':0.1,
     'seed':7,
@@ -103,9 +109,12 @@ exclude_features = [
     
     "prev_newyear",
     "post_newyear",
+    "lw_day",
+    "lw_type",
     "prev_lw",
     "post_lw",
     "post_christmas",
+    "prev_thanksgiving",
     "post_thanksgiving",
     
     "no_stock_days",
@@ -137,9 +146,12 @@ if "window_functions" in model_kwargs.keys():
                    for window_size in model_kwargs["window_sizes"]
                    for window_shift in model_kwargs["window_shifts"]]
     lagged_features.extend(rw_features)
-    
-lagged_features_to_dropna = [feat for feat in lagged_features if feat not in ["skew", "kurt"]]
+lagged_features_to_dropna = list(filter(lambda x: "skew" not in x, lagged_features))
+lagged_features_to_dropna = list(filter(lambda x: "kurt" not in x, lagged_features_to_dropna))
 
+###########################################################################################
+# model config
+###########################################################################################
 
 def objective(trial):
     sampled_params = {"num_leaves": trial.suggest_int("num_leaves", 63, 2047)}
@@ -147,25 +159,20 @@ def objective(trial):
     model_params = {**default_model_params, **sampled_params}
     model_kwargs["model_params"] = model_params
     
-    errors = list()
-    errors_l12 = list()
+    wrmsse_list = list()
+    wrmsseL12_list = list()
     
     for i,valid_period in enumerate(valid_periods):
-
         print("#"*100)
         print(f" Validation period: {valid_period} ".center(100, "#"))
         print("#"*100)
 
         valid_start = valid_period[0]
         valid_end = valid_period[1]
-        scales_level12 = compute_scaling(scaling_input, 
-                                         cut_date=valid_start,
-                                         agg_columns=["item_id","store_id"]).rename({"q":"s"}, axis=1)
-        weights_level12 = compute_weights(weighting_input, 
-                                          start_date=valid_start, level=12)
+        scales_level12 = all_scales[i]
+        weights_level12 = all_weights[i]
 
         stores_forecast = list()
-
         for store_id in range(1,11):
             print("-"*100)
             print(f" store_id: {store_id} ".center(100, "-"))
@@ -174,14 +181,11 @@ def objective(trial):
             _train_data = data.query("ds <= @valid_end & store_id == @store_id").reset_index(drop=True)
             _valid_index = _train_data.query("@valid_start <= ds <= @valid_end").index
 
-            if store_id in [1,2,3,4]:
-                # CA store
+            if store_id in [1,2,3,4]: # CA store
                 _train_data.drop(["snap_TX", "snap_TX_cum", "snap_WI", "snap_WI_cum"], axis=1, inplace=True)
-            elif store_id in [5,6,7]:
-                # TX store
+            elif store_id in [5,6,7]: # TX store
                 _train_data.drop(["snap_CA", "snap_CA_cum", "snap_WI", "snap_WI_cum"], axis=1, inplace=True)
-            else:
-                #WI store
+            else: #WI store
                 _train_data.drop(["snap_TX", "snap_TX_cum", "snap_CA", "snap_CA_cum"], axis=1, inplace=True)
 
             model_level12 = LightGBMForecaster(**model_kwargs)
@@ -189,12 +193,13 @@ def objective(trial):
             model_level12.train_features.dropna(subset=lagged_features_to_dropna, axis=0, inplace=True)
             model_level12.train_features = reduce_mem_usage(model_level12.train_features)
             model_level12.valid_features = reduce_mem_usage(model_level12.valid_features)
+            
             ts_id_in_train = model_level12.train_features.ts_id.unique()
             model_level12.valid_features = model_level12.valid_features.query("ts_id in @ts_id_in_train")
+            evaluator = Evaluator(model_level12.valid_features, weights_by_level, scales_by_level, single_store=True)
 
             print("Fitting the model")
             tic = time.time()
-            evaluator = WRMSSEEvaluatorL12(model_level12.valid_features, weights_level12, scales_level12)
             model_level12.fit(fit_kwargs={"verbose_eval":25, "feval":evaluator.evaluate})
             tac = time.time()
             print(f"Elapsed time: {(tac-tic)/60.} [min]")
@@ -206,9 +211,6 @@ def objective(trial):
             tac = time.time()
             print(f"Elapsed time: {(tac-tic)/60.} [min]")
 
-            wrmsse = evaluator._evaluate(forecast_v1.y_pred.values)
-            print("wrmsse:", wrmsse)
-
             forecast_v1["store_id"] = store_id
             stores_forecast.append(forecast_v1)
             del model_level12, _train_data, _valid_index, evaluator
@@ -217,14 +219,15 @@ def objective(trial):
         fold_forecast = pd.concat(stores_forecast, ignore_index=True)
         mrg = pd.merge(data.loc[:, ["ds","item_id","dept_id","cat_id","store_id","state_id","y"]],
                        fold_forecast, how="inner", on=["ds","item_id","store_id"])
-        evaluator = WRMSSEEvaluator(mrg.loc[:, ["ds","item_id","dept_id","cat_id","store_id","state_id","y"]])
-        wrmsse = evaluator._evaluate(mrg.y_pred.values)
+        evaluator = WRMSSEEvaluator(mrg.loc[:, ["ds","item_id","dept_id","cat_id","store_id","state_id","y"]], 
+                                    weight_by_level, scales_by_level)
         
-        errors.append(wrmsse)
-        errors_l12.append(evaluator.errors_by_level[("item_id","store_id")])
+        wrmsse = evaluator._evaluate(mrg.y_pred.values)
+        wrmsse_list.append(wrmsse)
+        wrmsseL12_list.append(evaluator.errors_by_level[("item_id","store_id")])
 
-    error = np.mean(errors_l12)
-    logger.write(f"{trial.number};{model_params};{errors};{errors_l12};{error}\n")
+    mean_error = np.mean(wrmsse_list)
+    logger.write(f"{trial.number};{model_params};{wrmsse_list};{wrmsseL12_list};{mean_error}\n")
     logger.flush()
     
     return error
@@ -233,7 +236,7 @@ def objective(trial):
 # study definition
 ###########################################################################################
 search_space = {
-    'num_leaves': [63, 127, 255, 511, 1023, 2047],
+    'num_leaves': [63, 127, 255, 511, 1023, 2047,],
     }
 study = optuna.create_study(direction='minimize', sampler=optuna.samplers.GridSampler(search_space))
 study.optimize(objective, n_trials=6)
